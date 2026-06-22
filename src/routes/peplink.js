213feadcd33056f29c8cdd4207d6626a2ab9d3e5
pegaz12 <suga12@gmail.com>
2026-06-22 13:46:50 +1000

    Update peplink.js

--- src/routes/peplink.js
+++ src/routes/peplink.js
@@ -7,27 +7,29 @@ const ORG_ID = process.env.PEPLINK_ORG_ID;
 const GROUP_ID = process.env.PEPLINK_GROUP_ID;
 
 function mapModem(modem) {
+  if (!modem) return {};
+
   return {
-    name: modem?.name,
-    carrier: modem?.carrier_name,
-    status: modem?.status,
-    ip: modem?.ip,
+    name: modem.name,
+    carrier: modem.carrier_name,
+    status: modem.status,
+    ip: modem.ip,
 
-    imei: modem?.imei,
-    iccid: modem?.sims?.[0]?.iccid,
-    imsi: modem?.sims?.[0]?.imsi,
-    apn: modem?.sims?.[0]?.apn || modem?.apn,
+    imei: modem.imei,
+    iccid: modem.sims?.[0]?.iccid,
+    imsi: modem.sims?.[0]?.imsi,
+    apn: modem.sims?.[0]?.apn || modem.apn,
 
-    rsrp: modem?.cellular_signals?.rsrp,
-    rsrq: modem?.cellular_signals?.rsrq,
-    sinr: modem?.cellular_signals?.sinr,
-    rssi: modem?.cellular_signals?.rssi,
+    rsrp: modem.cellular_signals?.rsrp,
+    rsrq: modem.cellular_signals?.rsrq,
+    sinr: modem.cellular_signals?.sinr,
+    rssi: modem.cellular_signals?.rssi,
 
-    band: modem?.gobi_band_class_name,
-    technology: modem?.data_technology || modem?.mobile_type,
+    band: modem.gobi_band_class_name,
+    technology: modem.data_technology || modem.mobile_type,
 
-    cellId: modem?.cell_tower?.cellId,
-    tac: modem?.cell_tower?.tac
+    cellId: modem.cell_tower?.cellId,
+    tac: modem.cell_tower?.tac
   };
 }
 
@@ -41,12 +43,12 @@ router.get("/devices", async (_req, res) => {
     const devices = result.data.map((device) => {
 
       const wan = device.interfaces?.find(
-        (i) => i.type === "ethernet" && i.enable
+        i => i.type === "ethernet" && i.enable
       );
 
       const cellularInterfaces =
         device.interfaces?.filter(
-          (i) => i.virtualType === "cellular"
+          i => i.virtualType === "cellular"
         ) || [];
 
       const modem1 = cellularInterfaces[0];
@@ -70,7 +72,8 @@ router.get("/devices", async (_req, res) => {
         temperature:
           device.temperature ||
           device.system_temperature ||
-          device.temp,
+          device.temp ||
+          null,
 
         clients: device.client_count,
 
@@ -93,12 +96,14 @@ router.get("/devices", async (_req, res) => {
         modem2: mapModem(modem2),
 
         wifi:
-          device.ssid_mac_list?.map((ssid) => ({
+          device.ssid_mac_list?.map(ssid => ({
             ssid: ssid.essid,
             radio: ssid.radio,
             enabled: ssid.enable,
             security: ssid.security
           })) || [],
+
+        // Legacy fields (keep for Base44 compatibility)
 
         carrier: modem1?.carrier_name,
         cellularStatus: modem1?.status,
@@ -110,6 +115,7 @@ router.get("/devices", async (_req, res) => {
         lastSeen: device.last_online
 
       };
+
     });
 
     res.json({
@@ -131,49 +137,69 @@ router.get("/devices", async (_req, res) => {
   }
 });
 
-router.get("/device/:id/clients-test", async (req, res) => {
+router.get("/device/:id/clients", async (req, res) => {
 
-  const id = req.params.id;
+  try {
 
-  const paths = [
-    `/rest/o/${ORG_ID}/g/${GROUP_ID}/d/${id}/clients`,
-    `/rest/o/${ORG_ID}/g/${GROUP_ID}/d/${id}/client`,
-    `/rest/o/${ORG_ID}/g/${GROUP_ID}/d/${id}/c`,
-    `/rest/o/${ORG_ID}/d/${id}/clients`,
-    `/rest/o/${ORG_ID}/d/${id}/client`
-  ];
+    const deviceId = req.params.id;
 
-  const results = [];
+    const result = await peplinkGet(
+      `/rest/o/${ORG_ID}/g/${GROUP_ID}/d/${deviceId}/client`
+    );
 
-  for (const path of paths) {
+    const clients = (result.data || []).map(client => ({
 
-    try {
+      name:
+        client.name ||
+        client.hostname ||
+        client.client_name ||
+        "Unknown",
 
-      const data = await peplinkGet(path);
+      ip:
+        client.ip ||
+        client.ip_addr ||
+        client.address,
 
-      results.push({
-        path,
-        ok: true,
-        data
-      });
+      mac:
+        client.mac ||
+        client.mac_addr,
 
-    } catch (e) {
+      interface:
+        client.interface ||
+        client.interface_name,
 
-      results.push({
-        path,
-        ok: false,
-        status: e.response?.status,
-        error:
-          typeof e.response?.data === "string"
-            ? e.response.data.substring(0, 150)
-            : e.message
-      });
+      type:
+        client.type ||
+        client.connection_type,
 
-    }
+      vendor: client.vendor,
+
+      connectedSince:
+        client.connected_since,
+
+      lastSeen:
+        client.last_seen
+
+    }));
+
+    res.json({
+      ok: true,
+      source: "peplink",
+      deviceId,
+      clients
+    });
+
+  } catch (err) {
+
+    console.error(err);
+
+    res.status(500).json({
+      ok: false,
+      source: "peplink",
+      error: err.response?.data || err.message
+    });
 
   }
-
-  res.json(results);
 
 });
 
